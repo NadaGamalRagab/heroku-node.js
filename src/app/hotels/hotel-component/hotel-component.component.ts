56eeb0a4d0c6620a083615a9b090cb8ee42224cb
nada <nadaragb367@gmail.com>
2021-01-29 19:09:06 +0200

    add hotel-component , hotels-listing

---
+++ src/app/hotels/hotel-component/hotel-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-component',
+  templateUrl: './hotel-component.component.html',
+  styleUrls: ['./hotel-component.component.scss']
+})
+export class HotelComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/hotels/hotels-listing/hotels-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hotels-listing',
+  templateUrl: './hotels-listing.component.html',
+  styleUrls: ['./hotels-listing.component.scss']
+})
+export class HotelsListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
